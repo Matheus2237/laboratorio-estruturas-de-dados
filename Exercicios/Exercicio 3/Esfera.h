df93d63a52a8769dd7053153d890e8df8c8a02f6
Matheus2237 <mathpaulinor@gmail.com>
2023-08-12 21:05:35 -0300

    docs: Inclusão da documentação da classe Esfera

--- Exercicios/Exercicio 3/Esfera.h
+++ Exercicios/Exercicio 3/Esfera.h
@@ -3,14 +3,42 @@
 
 #define PI 3.14159265359
 
+/**
+ * @brief Representação de uma esfera.
+ * 
+ */
 class Esfera {
 private:
     double raio;
 
 public:
+
+    /**
+     * @brief Constroi um novo objeto do tipo Esfera.
+     * 
+     * @param raio Raio da esfera
+     */
     Esfera(double raio);
+
+    /**
+     * @brief Recupera o raio do objeto.
+     * 
+     * @return double Valor do raio do objeto
+     */
     double getRaio();
+
+    /**
+     * @brief Calcula a área da esfera.
+     * 
+     * @return double Valor da área da esfera
+     */
     double calculaArea();
+
+    /**
+     * @brief Calcula o volume da esfera.
+     * 
+     * @return double Valor do volume da esfera.
+     */
     double calculaVolume();
 };
 
